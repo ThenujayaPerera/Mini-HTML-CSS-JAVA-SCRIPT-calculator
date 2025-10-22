40ec6d0c40a60ba7104d40857be8669cb0ac178e
S.S Thenujaya Perera <42-bcs-0011@kdu.ac.lk>
2025-10-23 00:16:41 +0530

    Update script.js
    adding js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 
+.calculator {
+    max-width: 300px;
+    margin: 20px auto;
+    padding: 10px;
+    border: 2px solid #333;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+.display {
+    width: 87%;
+    height: 50px;
+    margin: 10px;
+    font-size: 24px;
+    text-align: right;
+    padding: 5px;
+    border: 1px solid #ccc;
+    border-radius: 3px;
+    background-color: #ffffff;
+}
+.button {
+    width: 60px;
+    height: 40px;
+    margin: 5px;
+    font-size: 18px;
+    border: none;
+    border-radius: 3px;
+    background-color: #0c909a;
+    cursor: pointer;
+}
+.button:hover {
+    background-color: #05424f;
+}
+ 
+.button:active {
+    background-color: #d35400;
+}
